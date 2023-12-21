6c2826d988b137c2e41d1dd44d5fa4646ee53b89
Turi <40395348+turinaf@users.noreply.github.com>
2023-12-21 15:39:02 +0800

    remove old be read sql

--- be_read/fragment_be_read.py
+++ be_read/fragment_be_read.py
@@ -21,7 +21,7 @@ aid_list = [id[0] for id in aid]
 connection.close()
 
 # Read the SQL file
-with open('be_read_dbms2.sql', 'r') as file:
+with open('be_read/be_read_dbms2.sql', 'r') as file:
     sql_statements = file.read()
 
 # Find the start of the insert statement
@@ -57,7 +57,7 @@ with open('be_read_dbms1.sql', 'w') as file:
     file.write("DROP TABLE IF EXISTS `be_read`;\n")
     file.write("CREATE TABLE `be_read` (\n" + \
             "  `id` bigint NOT NULL AUTO_INCREMENT,\n" + \
-            "  `timestamp` char(14) DEFAULT NULL,\n" + \
+            "  `timestamp` text DEFAULT NULL,\n" + \
             "  `aid` char(7) DEFAULT NULL,\n" + \
             "  `readNum` bigint DEFAULT NULL,\n" +  \
             "  `readUidList` text,\n" +  \
@@ -72,24 +72,4 @@ with open('be_read_dbms1.sql', 'w') as file:
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n\n")
     file.write("LOCK TABLES `be_read` WRITE;\n")
     file.write('INSERT INTO `be_read` (`timestamp`, `aid`, `readNum`, `readUidList`, `commentNum`, `commentUidList`, `agreeNum`, `agreeUidList`, `shareNum`, `shareUidList`) VALUES\n' + "".join(dbms1_rows) + ';')
-    file.write("UNLOCK TABLES;\n\n\n")
-
-# # Write the dbms2 SQL file
-# with open('article_dbms2.sql', 'w') as file:
-#     file.write("DROP TABLE IF EXISTS `article`;\n")
-#     file.write("CREATE TABLE `article` (\n" + \
-#             "  `timestamp` char(14) DEFAULT NULL,\n" + \
-#             "  `id` char(7) DEFAULT NULL,\n" + \
-#             "  `aid` char(7) DEFAULT NULL,\n" + \
-#             "  `title` char(15) DEFAULT NULL,\n" +  \
-#             "  `category` char(11) DEFAULT NULL,\n" +  \
-#             "  `abstract` char(30) DEFAULT NULL,\n" +  \
-#             "  `articleTags` char(14) DEFAULT NULL,\n" +  \
-#             "  `authors` char(13) DEFAULT NULL,\n" +  \
-#             "  `language` char(3) DEFAULT NULL,\n" +  \
-#             "  `text` char(31) DEFAULT NULL,\n" +  \
-#             "  `image` char(32) DEFAULT NULL,\n" +  \
-#             "  `video` char(32) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8;\n\n")
-#     file.write("LOCK TABLES `article` WRITE;\n")
-#     file.write('INSERT INTO `article` VALUES\n' + ''.join(dbms2_rows) + ';')
-#     file.write("UNLOCK TABLES;\n\n\n")
+    file.write("UNLOCK TABLES;")
